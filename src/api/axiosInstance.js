13f59477261d7b2772adc132d57e86495afd4891
shams-wael <shamswael152@gmail.com>
2026-07-18 18:26:26 +0400

    Fix login authentication

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,16 +1,24 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://e-commerce-api-3wara.vercel.app', 
+  baseURL: 'https://e-commerce-api-3wara.vercel.app',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
-api.interceptors.request.use((config) => {
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjZhNDNjYmQ0MzMwYTZjN2ZkYWZlOTc1ZiIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTc4MzU2MTY5NCwiZXhwIjoxNzgzOTkzNjk0fQ.pbcJKo6R3cwfMp-H5wJ95SVDk8KJhR92vV2C2z8N8Og";
-  
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+
+
+    if (token && config.url !== '/auth/login') {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
