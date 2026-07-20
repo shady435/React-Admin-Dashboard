17da575cefeb27e2a5698f7910af4b1aef25a7ca
shams-wael <shamswael152@gmail.com>
2026-07-20 10:37:18 +0400

    Fix Vercel and GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   plugins: [tailwindcss(), react()],
-  
-  // التعديل الجديد عشان نثبت البورت
+
+  base: process.env.VERCEL ? "/" : "/Dashboardd/",
+
   server: {
     port: 5173,
-    strictPort: true, 
-  }
-})
+    strictPort: true,
+  },
+}));
